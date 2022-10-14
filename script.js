ea18a9c7799c2a10000e49a0e6159114ce679f45
FalgasDev <fgarrote12@gmail.com>
2022-10-14 14:45:12 -0300

    Adicionada a funcionalidade de fechar o pedido e o innerHTML

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const prato = document.querySelector('.pratos')
 const bebida = document.querySelector('.bebidas')
 const sobremesa = document.querySelector('.sobremesas')
+let pratoPreco;
+let bebidaPreco;
+let sobremesaPreco;
+let pratoEscolhido;
+let bebidaEscolhido;
+let sobremesaEscolhido;
 let pratoSelecionado;
 let bebidaSelecionado;
 let sobremesaSelecionado;
@@ -9,10 +15,15 @@ function selecionarPrato(x) {
     if (pratoSelecionado) {
         pratoSelecionado.classList.remove('selecionado')
         x.classList.add('selecionado')
+        pratoSelecionado = prato.querySelector('.selecionado')
     } else {
         x.classList.add('selecionado')
         pratoSelecionado = prato.querySelector('.selecionado')
     }
+    pratoPreco = pratoSelecionado.querySelector('span')
+    pratoPreco = Number(pratoPreco.innerHTML)
+    pratoEscolhido = pratoSelecionado.querySelector('p')
+    pratoEscolhido = pratoEscolhido.innerHTML
     finalizarPedido()
 }
 
@@ -20,10 +31,15 @@ function selecionarBebida(x) {
     if (bebidaSelecionado) {
         bebidaSelecionado.classList.remove('selecionado')
         x.classList.add('selecionado')
+        bebidaSelecionado = bebida.querySelector('.selecionado')
     } else {
         x.classList.add('selecionado')
         bebidaSelecionado = bebida.querySelector('.selecionado')
     }
+    bebidaPreco = bebidaSelecionado.querySelector('span')
+    bebidaPreco = Number(bebidaPreco.innerHTML)
+    bebidaEscolhido = bebidaSelecionado.querySelector('p')
+    bebidaEscolhido = bebidaEscolhido.innerHTML
     finalizarPedido()
 }
 
@@ -31,10 +47,15 @@ function selecionarSobremesa(x) {
     if (sobremesaSelecionado) {
         sobremesaSelecionado.classList.remove('selecionado')
         x.classList.add('selecionado')
+        sobremesaSelecionado = sobremesa.querySelector('.selecionado')
     } else {
         x.classList.add('selecionado')
         sobremesaSelecionado = sobremesa.querySelector('.selecionado')
     }
+    sobremesaPreco = sobremesaSelecionado.querySelector('span')
+    sobremesaPreco = Number(sobremesaPreco.innerHTML)
+    sobremesaEscolhido = sobremesaSelecionado.querySelector('p')
+    sobremesaEscolhido = sobremesaEscolhido.innerHTML
     finalizarPedido()
 }
 
@@ -43,6 +64,19 @@ function finalizarPedido() {
     if (pratoSelecionado && bebidaSelecionado && sobremesaSelecionado) {
         finalizar.classList.add('verde')
         finalizar.innerHTML = 'Fechar pedido'
+    }
+}
+
+function fazerPedido() {
+    const valorTotal = pratoPreco + bebidaPreco + sobremesaPreco
+    if (pratoSelecionado && bebidaSelecionado && sobremesaSelecionado) {
+        const mensagem = `Olá, gostaria de fazer o pedido:
+        - Prato: ${pratoEscolhido} 
+        - Bebida: ${bebidaEscolhido} 
+        - Sobremesa: ${sobremesaEscolhido} 
+        Total: R$ ${valorTotal}`
+        const url = `https://wa.me/5511964287262?text=${encodeURIComponent(mensagem)}`;
+        window.open(url);
     }
 }
 
