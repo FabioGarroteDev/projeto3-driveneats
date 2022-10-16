16848eb43fb47cee2517f9e50daea8f51bb28f78
FabioGarroteDev <fgarrote12@gmail.com>
2022-10-16 05:07:02 -0300

    Adicionado o card flutuante de revisar pedido

--- script.js
+++ script.js
@@ -60,30 +60,66 @@ function selecionarSobremesa(x) {
 }
 
 function finalizarPedido() {
-    const finalizar = document.querySelector('button')
+    const finalizar = document.querySelector('.fechar-pedido button')
     if (pratoSelecionado && bebidaSelecionado && sobremesaSelecionado) {
         finalizar.classList.add('verde')
         finalizar.innerHTML = 'Fechar pedido'
     }
 }
 
+function revisarPedido() {
+    let valorTotal = pratoPreco + bebidaPreco + sobremesaPreco
+    valorTotal = valorTotal.toFixed(2)
+    const opaco = document.querySelector('.fundo-opaco')
+    const revisar = document.querySelector('.card-flutuante')
+    const nomePrato = revisar.querySelector('.valores div p')
+    const valorPrato = revisar.querySelector('.valores div h3')
+    const nomeBebida = revisar.querySelector('.valores div:nth-child(2) p')
+    const valorBebida = revisar.querySelector('.valores div:nth-child(2) h3')
+    const nomeSobremesa = revisar.querySelector('.valores div:nth-child(3) p')
+    const valorSobremesa = revisar.querySelector('.valores div:nth-child(3) h3')
+    const total = revisar.querySelector('.valores div:nth-child(4) h3')
+    let pratoPrecoFixed = Number(pratoPreco)
+    pratoPrecoFixed = pratoPrecoFixed.toFixed(2)
+    let bebidaPrecoFixed = Number(bebidaPreco)
+    bebidaPrecoFixed = bebidaPrecoFixed.toFixed(2)
+    let sobremesaPrecoFixed = Number(sobremesaPreco)
+    sobremesaPrecoFixed = sobremesaPrecoFixed.toFixed(2)
+    if (pratoSelecionado && bebidaSelecionado && sobremesaSelecionado) {
+        opaco.classList.remove('escondido')
+        revisar.classList.remove('escondido')
+    }
+    nomePrato.innerHTML = pratoEscolhido
+    valorPrato.innerHTML = pratoPrecoFixed
+    nomeBebida.innerHTML = bebidaEscolhido
+    valorBebida.innerHTML = bebidaPrecoFixed
+    nomeSobremesa.innerHTML = sobremesaEscolhido
+    valorSobremesa.innerHTML = sobremesaPrecoFixed
+    total.innerHTML = valorTotal
+}
+
 function fazerPedido() {
     let valorTotal = pratoPreco + bebidaPreco + sobremesaPreco
     valorTotal = valorTotal.toFixed(2)
-    if (pratoSelecionado && bebidaSelecionado && sobremesaSelecionado) {
-        const nome = prompt('Qual o seu nome?')
-        const endereco = prompt('Qual o seu endereço?')
-        const mensagem = `Olá, gostaria de fazer o pedido:
-        - Prato: ${pratoEscolhido} 
-        - Bebida: ${bebidaEscolhido} 
-        - Sobremesa: ${sobremesaEscolhido} 
-        Total: R$ ${valorTotal}
+    const nome = prompt('Qual o seu nome?')
+    const endereco = prompt('Qual o seu endereço?')
+    const mensagem = `Olá, gostaria de fazer o pedido:
+    - Prato: ${pratoEscolhido} 
+    - Bebida: ${bebidaEscolhido} 
+    - Sobremesa: ${sobremesaEscolhido} 
+    Total: R$ ${valorTotal}
         
-        Nome: ${nome}
-        Endereço: ${endereco}`
-        const url = `https://wa.me/5511964287262?text=${encodeURIComponent(mensagem)}`;
-        window.open(url);
-    }
+    Nome: ${nome}
+    Endereço: ${endereco}`
+    const url = `https://wa.me/5511964287262?text=${encodeURIComponent(mensagem)}`;
+    window.open(url);
+}
+
+function cancelar() {
+    const fechar = document.querySelector('.card-flutuante')
+    const sairFundo = document.querySelector('.fundo-opaco')
+    fechar.classList.add('escondido')
+    sairFundo.classList.add('escondido')
 }
 
 
